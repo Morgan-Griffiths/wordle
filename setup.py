7f9e36ca6d12ecbf501cbcaf6694b4505ccd64c1
Morgan <c5ipo7i@yahoo.com>
2022-04-27 18:22:51 -0700

    chore: added weight and result folders

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from pathlib import Path
 
 Path("dataset").mkdir(parents=True, exist_ok=True)
 Path("weights").mkdir(parents=True, exist_ok=True)
+Path("results").mkdir(parents=True, exist_ok=True)
 Path("assets").mkdir(parents=True, exist_ok=True)
