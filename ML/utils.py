b50e0779c22386411e42f65aed12e69bf3d20a74
Morgan <c5ipo7i@yahoo.com>
2022-04-29 22:29:12 -0700

    feat: added weight utils for going back and forth between dataparallel and cpu

--- ML/utils.py
+++ ML/utils.py
@@ -1,5 +1,6 @@
 import torch
 import os
+from collections import OrderedDict 
 
 
 def reward_over_states(last_turn: torch.tensor):
@@ -29,3 +30,39 @@ def save_weights(network, path):
 def hard_update(source, target):
     for target_param, param in zip(target.parameters(), source.parameters()):
         target_param.data.copy_(param.data)
+
+def strip_module(state_dict):
+    # state_dict = load_path(path)
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        if k.find('module') > -1:
+            name = ''.join(k.split('.module'))
+            new_state_dict[name] = v
+        else:
+            new_state_dict[k] = v
+    return new_state_dict
+
+def add_module(path):
+    state_dict = load_path(path)
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = 'module.'+k
+        new_state_dict[name] = v
+    return new_state_dict
+
+def is_path_ddp(path):
+    is_ddp = False
+    state_dict = load_path(path)
+    for k in state_dict.keys():
+        if k.find('module.') > -1:
+            is_ddp = True
+        break
+    return is_ddp
+
+def is_net_ddp(net):
+    is_ddp = False
+    for name,param in net.named_parameters():
+        if name.find('module.') > -1:
+            is_ddp = True
+        break
+    return is_ddp
