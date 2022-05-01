bd9b0cd5c8ca8c538c2c93249cac5630ca7774bc
Morgan <c5ipo7i@yahoo.com>
2022-05-01 13:41:37 -0700

    chore: fixing policy to work with new training style

--- train_policy.py
+++ train_policy.py
@@ -1,16 +1,33 @@
 from collections import deque
 import sys
 import torch
+import torch.nn.functional as F
 from torch import optim
 import numpy as np
-from globals import index_result_dict, PolicyOutputs
+from globals import index_result_dict, PolicyOutputs,CHECKPOINT
+from ray_files.replay_buffer import ReplayBuffer
 from experiments.generate_data import load_data
 from experiments.globals import actionSpace, DataTypes, NetworkConfig, dataMapping
 from torch.nn import SmoothL1Loss
+from config import Config
+from main import load_replay_buffer
+import copy
+import ray
 
-
-def test_policy(agent_params, training_params, network_params, dataset):
-    target_actions, target_rewards = dataset["trainY"]
+def test_policy(agent_params, training_params, network_params, per_buffer):
+    next_batch = per_buffer.get_batch.remote()
+    index_batch, batch = ray.get(next_batch)
+    (
+        state_batch,
+        action_batch,
+        value_batch,
+        reward_batch,
+        policy_batch,
+        weight_batch,
+        result_batch,
+        word_batch,
+        gradient_scale_batch,
+    ) = batch
     # print("target_actions", target_actions)
     # print("target_rewards", target_rewards)
     net = agent_params["network"](network_params)
@@ -24,14 +41,12 @@ def test_policy(agent_params, training_params, network_params, dataset):
     for epoch in range(training_params["epochs"]):
         sys.stdout.write("\r")
         optimizer.zero_grad()
-        states = dataset["trainX"]
-        outputs: PolicyOutputs = net(torch.as_tensor(states).long())
-        target_actions, target_rewards = dataset["trainY"]
+        outputs: PolicyOutputs = net(state_batch)
         # print("outputs.probs", outputs.probs)
         # print("outputs.value", outputs.value)
-        policy_loss = criterion(outputs.probs, torch.as_tensor(target_actions))
+        policy_loss = F.nll_loss(outputs.logprobs, word_batch)
         value_loss = value_criterion(
-            outputs.value, torch.as_tensor(target_rewards).float()
+            outputs.value, reward_batch
         )
         loss = policy_loss + value_loss
         loss.backward()
@@ -49,19 +64,30 @@ def test_policy(agent_params, training_params, network_params, dataset):
     # return np.mean(score_window)
 
 
-def validation(network, dataset):
+def validation(network, per_buffer):
+    next_batch = per_buffer.get_batch.remote()
+    index_batch, batch = ray.get(next_batch)
+    (
+        state_batch,
+        action_batch,
+        value_batch,
+        reward_batch,
+        policy_batch,
+        weight_batch,
+        result_batch,
+        word_batch,
+        gradient_scale_batch,
+    ) = batch
     while True:
-        states = dataset["trainX"]
-        target_actions, rewards = dataset["trainY"]
-        print(f"Number of states {len(states)}")
+        print(f"Number of states {len(state_batch)}")
         try:
-            sample_idx = int(input(f"Pick a number between 0-{len(states)}"))
+            sample_idx = int(input(f"Pick a number between 0-{len(state_batch)}"))
         except Exception as e:
             print(e)
         # sample_idx = np.random.choice(len(states))
-        state = states[sample_idx][None, :]
-        target_action = target_actions[sample_idx]
-        target_reward = rewards[sample_idx]
+        state = state_batch[sample_idx][None, :]
+        target_action = word_batch[sample_idx]
+        target_reward = reward_batch[sample_idx]
 
         state = torch.as_tensor(state).long()
         print("state", state, state.shape)
@@ -107,6 +133,7 @@ if __name__ == "__main__":
     parser.set_defaults(validate=False)
 
     args = parser.parse_args()
+    config = Config()
     print(args)
     network_path = "results/policy_test"
     loss_type = dataMapping[args.datatype]
@@ -130,19 +157,17 @@ if __name__ == "__main__":
         "load_path": network_path,
         "emb_size": 16,
     }
-    dataset = load_data(args.datatype)
+    config.batch_size = 4
+    buffer_info = load_replay_buffer()
+    checkpoint = copy.copy(CHECKPOINT)
+    checkpoint["num_played_steps"] = buffer_info["num_played_steps"]
+    checkpoint["num_played_games"] = buffer_info["num_played_games"]
+    checkpoint["num_reanalysed_games"] = buffer_info["num_reanalysed_games"]
+    per_buffer = ReplayBuffer.remote(checkpoint, buffer_info["buffer"], config)
     if args.validate:
         net = agent_params["network"](network_params)
         net.load_state_dict(torch.load(network_path))
         net.eval()
-        validation(net, dataset)
+        validation(net, per_buffer)
     else:
-        states = np.load("states.npy")
-        actions = np.load("actions.npy")
-        rewards = np.load("reward_targets.npy")[:, None]
-        print(states.shape)
-        print(actions.shape)
-        print(rewards.shape)
-        print(sum(rewards))
-        dataset = {"trainX": states, "trainY": (actions, rewards)}
-        test_policy(agent_params, training_params, network_params, dataset)
+        test_policy(agent_params, training_params, network_params, per_buffer)
