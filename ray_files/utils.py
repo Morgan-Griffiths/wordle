eb3b2c83c3080f9b10d4b6ffb2f33789fed8875b
Morgan <c5ipo7i@yahoo.com>
2022-05-07 23:17:04 -0700

    feat: added loading pre trained dynamics function

--- ray_files/utils.py
+++ ray_files/utils.py
@@ -1,5 +1,6 @@
 from ML.networks import MuZeroNet
 import ray
+import torch
 
 
 @ray.remote(num_cpus=0, num_gpus=0)
@@ -10,6 +11,13 @@ class CPUActor:
 
     def get_initial_weights(self, config):
         model = MuZeroNet(config)
+        if config.load_dynamic_weights:
+            print(f"Loading dynamic weights from  {config.dynamics_weight_path}")
+            checkpoint = torch.load(config.dynamics_weight_path, map_location="cpu")
+            dynamic_dict = model._dynamics.state_dict()
+            pretrained_dict = {k[10:]:v for k, v in checkpoint['weights'].items() if k.find('_dynamics') > -1}
+
+            model._dynamics.load_state_dict(pretrained_dict)
         weights = model.get_weights()
         summary = str(model).replace("\n", " \n\n")
         return weights, summary
