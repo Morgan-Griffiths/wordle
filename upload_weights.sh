c75f94b5d9edd992517ebd667a42883015b1d101
Morgan <c5ipo7i@yahoo.com>
2022-05-19 12:35:26 -0700

    adjusted upload/download and added log folder to setup

--- upload_weights.sh
+++ upload_weights.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Uploads public folder to S3
-aws s3 cp ./weights/production s3://wordle-ai/production --acl public-read --cache-control max-age=0
+aws s3 cp ./weights/production.checkpoint s3://wordle-ai/production --acl public-read --cache-control max-age=0
