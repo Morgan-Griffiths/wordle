57ec8cca7301eb7bea1de6c5f215b20833a6b69e
Morgan <c5ipo7i@yahoo.com>
2022-05-19 13:31:03 -0700

    chore: adjusted upload/download. started on reducing early stage action space

--- download_weights.sh
+++ download_weights.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aws s3 sync s3://wordle-ai ${PWD}/weights/production.checkpoint --no-sign-request
+aws s3 sync s3://wordle-ai ${PWD}/weights/production.checkpoint --no-sign-request --profile personal
